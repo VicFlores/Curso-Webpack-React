ce365b5dc2bd776ca20e89bda167e2dd72f4c429
Vic Flores <vicsito2014@gmail.com>
2021-04-01 09:33:04 -0600

    Desplegando en Netlify

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,26 +1,18 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CssMinimizerPlugin = require('mini-css-extract-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 /** @type {import('webpack').Configuration} */
 
 module.exports = {
   entry: './src/index.js',
-  mode: 'production',
+  mode: 'development',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: '/',
   },
   resolve: {
     extensions: ['.js', '.jsx'],
-    alias: {
-      '@components': path.resolve(__dirname, './src/components'),
-      '@styles': path.resolve(__dirname, './styles/'),
-    },
   },
   module: {
     rules: [
@@ -49,10 +41,11 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: '[name].css',
     }),
-    new CleanWebpackPlugin(),
   ],
-  optimization: {
-    minimize: true,
-    minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    open: true,
+    port: 3000,
   },
 };
